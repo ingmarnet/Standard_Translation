5b65dd11a0fedfe458e560ddbb7c949abaf81518
ingmarnet <ingmar.net@gmail.com>
2026-02-24 21:06:25 -0300

    Initial commit: Standard_Translation module structure

---
+++ app/code/Standard/Translation/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © Standard. All rights reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Standard_Translation',
+    __DIR__
+);
